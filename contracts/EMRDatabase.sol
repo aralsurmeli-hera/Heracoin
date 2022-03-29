8840a45642b2d232bbe3a9ac0de5f1abf26842b4
austinhatch <32887233+austinhatch@users.noreply.github.com>
2022-03-28 21:35:53 -0400

    Update EMRDatabase.sol

--- contracts/EMRDatabase.sol
+++ contracts/EMRDatabase.sol
@@ -23,22 +23,18 @@ contract EMRDatabase is Ownable, AccessControl {
     }
 
     //Creates an event for a new record being created
-    event recordCreated(address patient, int record_id);
+    event EmrCreated(address patient, int record_id);
+    event SentRewardTokens(uint256 amount, address fromAccount, uint256 totalBalance);
+    event EmrAccessed(address add);
     
-    event sentRewardTokens(uint256 amount, address fromAccount, uint256 totalBalance);
 
     //Creates Modifier that checks for the owner of the EMR
     modifier isEMROwner(address calldata add, EMR calldata emr){
         require( emr.owner == add);
         _;
     }
-
-    //Creates a mapping of patients to the IDs of their EMRs
-    mapping (address => int[]) accountsToEMRs;
-
-    //An array of all EMRs
-    EMR[] emrDatabase; 
-
+    
+    //Creates the EMR Struct
     struct EMR {
         uint256 id;
         address owner;
@@ -49,11 +45,16 @@ contract EMRDatabase is Ownable, AccessControl {
         bytes32 ifps_hash;
     }
 
-    function setRewardAmount(uint256 reward) public isOwner(msg.sender) returns (bool){
-        reward_ammount = reward;
-        return true;
-    } 
+    //An array of all EMRs
+    EMR[] emrDatabase; 
+    
+    //Creates a mapping of patients to the indexes of their EMRs in the emrDatabase
+    mapping (address => int[]) ownersToEMRs;
+    
+    //Creates a mapping of addresses to the indexes of EMRs in the ermDatabase
+    mapping (address => int[]) accessorsToEMRs;
 
+    //Creates an 
     function createEMR(bytes32 _record_type, bytes32 _record_status, uint256 _record_date, bytes32 _ifps_hash) public returns(bool){
         EMR emr = new EMR();
         emr.id = emrDatabase.length;
@@ -62,25 +63,39 @@ contract EMRDatabase is Ownable, AccessControl {
         emr.record_date = _record_date;
         emr.record_type = _record_type;
         emr.ifps_hash = _ifps_hash;
+        
+        //Adds the EMR to the Database
+        emrDatabase.push(emr);
+        
+        //Maps the new EMR to the Patient Address
+        ownersToEMRs[msg.sender].push(emr.id);
+        
+        //Emits the creation event to the blockchain
+        emit EMRCreated(msg.sender, emr.id);
+        
+        //Rewards the patient for creating an EMR
+        sendRewardForEmrCreation(msg.sender);s
     }
 
-    function getEMR(int id) public returns(EMR) {
-        require(emrDatabase[id].owner == msg.sender);
+    function getOwnedEMR(int id) public returns(EMR) {
         return emrDatabase[id];
     }
-
+    
+    function getOwnedEMRsArray(address patient) public returns(EMR[]){
+        return ownersToEMRs[patient];
+    }
+    
     function getEMRHash(EMR emr) public isEMROwner(msg.sender,emr){
         return emr.ifps_hash;
     }
-
-     function getEmrJSON(uint256 vaccineID) override(ERC721) public view returns (string memory) {
-        string memory json = Base64.encode(
-            bytes(string(
-                abi.encodePacked(
-                    //Fill in FHIR JSON Format to return for Vaccine here
-                )
-            ))
-        );
-        return string(abi.encodePacked('data:application/json;base64,', json));
-    }    
+    
+    function setRewardAmount(uint256 reward) public isOwner(msg.sender) returns (bool){
+        reward_ammount = reward;
+        return true;
+    } 
+    
+    function sendRewardForEmrCreation(address patient) private { 
+        heracoin.transferFrom(address(this), patient, this.reward_amount);
+        emit SentRewardTokens(this.reward_amount, address(this), patient, heracoin.balanceOf(address(this));
+    }
 }

61aeb235f16e50f3b43fc661857e689d2f6e72c4
austinhatch <32887233+austinhatch@users.noreply.github.com>
2022-04-20 22:30:51 -0400

    Update AccountAdministrator.sol

--- contracts/AccountAdministrator.sol
+++ contracts/AccountAdministrator.sol
@@ -1,26 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0;
+pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0-solc-0.7/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AccountAdministrator is Ownable{
-
-    address owner;
-
+contract AccountAdministrator is Ownable {
     mapping(address => bytes32) userAddresses;
 
     //Create a mapping of the Blockchain address to the encrypted userID from Hera App
-    function mapNewAddress(bytes32 _address, bytes32 _userhash) public onlyOwner{
-        address a = _address;
+    function mapNewAddress(address a, bytes32 _userhash) public onlyOwner {
         userAddresses[a] = _userhash;
     }
 
-    function getUserFromAddress(address a) public onlyOwner returns(bytes32){
+    function getUserFromAddress(address a) public onlyOwner returns (bytes32) {
         return userAddresses[a];
-    } 
-    
-    function removeUserFromAddress(address a) public onlyOwner{
-        delete userAddresses[a];
     }
 
+    function removeUserFromAddress(address a) public onlyOwner {
+        delete userAddresses[a];
+    }
 }

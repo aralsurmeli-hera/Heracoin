f528a5989a26340cb815f5f580d8637b873ca125
austinhatch <32887233+austinhatch@users.noreply.github.com>
2022-04-20 22:29:30 -0400

    Update HeraCoin.sol

--- contracts/HeraCoin.sol
+++ contracts/HeraCoin.sol
@@ -1,11 +1,10 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0-solc-0.7/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract HeraCoin is ERC20 {
-
-    constructor () ERC20("HERACoin", "HERA") public {
-        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    constructor() public ERC20("HERACoin", "HERA") {
+        _mint(msg.sender, 1000000 * (10**uint256(decimals())));
     }
 }
+

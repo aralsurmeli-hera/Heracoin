3f40f5c3a9bcf6baa6de2605980039bde9ef6961
austinhatch <32887233+austinhatch@users.noreply.github.com>
2022-06-15 22:21:53 -0400

    Add files via upload

--- herachain-demo/scripts/deploy.js
+++ herachain-demo/scripts/deploy.js
@@ -1,32 +1,33 @@
 
 const hre = require("hardhat");
 const fs = require('fs');
-const { heracoinAddress } = require("../src/config");
 
 async function main() {
 
   const HeraCoinFactory = await hre.ethers.getContractFactory("HeraCoin");
   const HeraCoin = await HeraCoinFactory.deploy();
   const HeraCoinAddress = await HeraCoin.deployed();
+  console.log("HeraCoin deployed to:", HeraCoinAddress.address);
 
   const RewarderFactory = await hre.ethers.getContractFactory("HeraCoinRewarder");
   const Rewarder = await RewarderFactory.deploy(HeraCoinAddress.address);
   const RewarderAddress = await Rewarder.deployed();
+  console.log("Rewarder deployed to:", RewarderAddress.address);
+
+  await HeraCoin.transfer(RewarderAddress.address, "100000000000000000");
+  console.log("Rewarder has been funded");
 
   const EMRDatabaseFactory = await hre.ethers.getContractFactory("EMRContractDatabase");
   const EMRDatabase = await EMRDatabaseFactory.deploy(RewarderAddress.address);
   const EMRDatabaseAddress = await EMRDatabase.deployed();
-
   console.log("EMR Database deployed to:", EMRDatabaseAddress.address);
-  console.log("HeraCoin deployed to:", HeraCoinAddress.address);
-  console.log("Rewarder deployed to:", RewarderAddress.address);
 
-  fs.writeFileSync('./src/config.js', `
-  export const databaseAddress = "${EMRDatabaseAddress.address}"
-  export const heracoinAddress = "${HeraCoinAddress.address}"
-  export const rewarderAddress = "${RewarderAddress.address}"
-  export const ownerAddress = "${EMRDatabaseAddress.signer.address}"
-  `)
+  fs.writeFileSync('./src/config.js',
+    `export const databaseAddress = "${EMRDatabaseAddress.address}"
+    export const heracoinAddress = "${HeraCoinAddress.address}"
+    export const rewarderAddress = "${RewarderAddress.address}"
+    export const ownerAddress = "${EMRDatabaseAddress.signer.address}"`
+    , { encoding: 'utf8', flag: 'w' })
 
 }
 

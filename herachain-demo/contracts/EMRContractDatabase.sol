e2ba573851f4c17047bfd036289d734cd187bc7f
austinhatch <ahatch@colgate.edu>
2022-07-06 22:05:32 -0400

    Reconfigured the Contract Structure to use a 1:1 Patient to EMRStorageContract that holds all Document structs for a Patient

--- herachain-demo/contracts/EMRContractDatabase.sol
+++ herachain-demo/contracts/EMRContractDatabase.sol
@@ -5,23 +5,14 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./HeraCoinRewarder.sol";
-import "./EMRContract.sol";
+import "./EMRStorageContract.sol"; 
 
 contract EMRContractDatabase {
     //Reward Contract
     HeraCoinRewarder private rewarder;
 
     //Mapping of a patient's address to the integer IDs of their EMRContracts
-    mapping(address => uint256[]) public ownersToEMRs;
-
-    //Mapping the IDs of a contract to the address of the EMR
-    mapping(uint256 => EMRContract) private idsToEMRs;
-
-    //Mapping the IDs of a contract to the address of the owner
-    mapping(uint256 => address) private idsToOwners;
-
-    using Counters for Counters.Counter;
-    Counters.Counter private _EMRIds;
+    mapping(address => address) public ownersToEMRStorage;
 
     event EMRCreated(address patient, uint256 record_id);
     event SentRewardTokens(
@@ -30,58 +21,71 @@ contract EMRContractDatabase {
         address toAccount,
         uint256 totalBalance
     );
-    event EMRAccessed(address accessor, uint256 record_id);
 
     constructor(HeraCoinRewarder _rewarder) {
         rewarder = _rewarder;
     }
 
-    function createEMR(
-        string memory _record_type,
-        string memory _record_status,
-        uint256 _record_date,
-        string memory _ipfs_image_hash,
-        string memory _ipfs_data_hash
-    ) public returns (bool) {
-        //The database can build the EMRContract
-        EMRContract new_emr = new EMRContract(
-            _record_type,
-            _record_status,
-            _record_date,
-            _ipfs_image_hash,
-            _ipfs_data_hash
-        );
+    function createEMRStorage() public returns (bool) {
+        //The database can build the EMRStorageContract
+        if(ownersToEMRStorage[msg.sender] != address(0)){
+            revert("An EMRStorageContract already exists for this address");
+        }
+
+        EMRStorageContract new_emr_storage = new EMRStorageContract(this);
 
         //Then transfer its ownership to the patient
-        new_emr.transferOwnership(msg.sender);
+        new_emr_storage.transferOwnership(msg.sender);
 
-        uint256 newId = _EMRIds.current();
-        _EMRIds.increment();
-
-        idsToEMRs[newId] = new_emr;
-
-        //Maps the new EMR to the Patient Address
-        ownersToEMRs[msg.sender].push(newId);
-
-        //Emits the creation event to the blockchain
-        emit EMRCreated(msg.sender, newId);
+        //Maps the new EMRStorage to the Patient Address
+        ownersToEMRStorage[msg.sender] = address(new_emr_storage);
 
         //Rewards the patient for creating an EMR using the Rewarder contract
-        rewarder.sendRewardForEmrCreation(payable(msg.sender));
         return true;
     }
 
-    function getEMRById(uint256 _id) public view returns (EMRContract) {
-        return idsToEMRs[_id];
+    function sendRewardForEmrCreation(address patient) public {
+        rewarder.sendRewardForEmrCreation(payable(patient));
     }
 
-    //Returns an number of owned EMRs for the address that calls the function
-    function getNumberOwnedEMRs() public view returns (uint256 count) {
-        return ownersToEMRs[msg.sender].length;
-    }
+    // function createEMR(
+    //     string memory _record_type,
+    //     string memory _record_status,
+    //     uint256 _record_date,
+    //     string memory _ipfs_image_hash,
+    //     string memory _ipfs_data_hash
+    // ) public returns (bool) {
+    //     //The database can build the EMRContract
+    //     EMRContract new_emr = new EMRContract(
+    //         _record_type,
+    //         _record_status,
+    //         _record_date,
+    //         _ipfs_image_hash,
+    //         _ipfs_data_hash
+    //     );
 
-    //Returns an array of the IDs of owned EMRs for the address that calls the function
-    function getOwnedEMRsArray() public view returns (uint256[] memory) {
-        return ownersToEMRs[msg.sender];
+    //     //Then transfer its ownership to the patient
+    //     new_emr.transferOwnership(msg.sender);
+
+    //     uint256 newId = _EMRIds.current();
+    //     _EMRIds.increment();
+
+    //     idsToEMRs[newId] = new_emr;
+
+    //     //Maps the new EMR to the Patient Address
+    //     ownersToEMRs[msg.sender].push(newId);
+
+    //     //Emits the creation event to the blockchain
+    //     emit EMRCreated(msg.sender, newId);
+
+    //     //Rewards the patient for creating an EMR using the Rewarder contract
+    //     rewarder.sendRewardForEmrCreation(payable(msg.sender));
+    //     return true;
+    // }
+
+    
+
+    function getEMRStorageContract() public view returns (address){
+        return ownersToEMRStorage[msg.sender];
     }
 }
---
+++ herachain-demo/contracts/EMRStorageContract.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./EMRContractDatabase.sol";
+
+struct EMR {
+    string record_type;
+    string record_status;
+    uint256 record_date;
+    uint256 publish_date;
+    string ipfs_image_hash;
+    string ipfs_data_hash;
+}
+
+contract EMRStorageContract is Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private emrIdCounter;
+
+    event EMRCreated(address patient, uint256 record_id);
+
+    event EMRAccessed(address accessor, uint256 record_id);
+
+    mapping(uint256 => EMR) private emrs;
+    uint256[] private emrIds;
+
+    EMRContractDatabase database;
+
+    constructor(EMRContractDatabase _database) {
+        database=_database;
+    }
+
+    function addRecord(
+        string memory _record_type,
+        string memory _record_status,
+        uint256  _record_date,
+        uint256  _publish_date,
+        string memory _ipfs_image_hash,
+        string memory _ipfs_data_hash
+    ) public onlyOwner {
+
+        uint256 _recordId = emrIdCounter.current();
+        emrs[_recordId] = EMR(
+            _record_type,
+            _record_status,
+            _record_date,
+            _publish_date,
+            _ipfs_image_hash,
+            _ipfs_data_hash
+
+        );
+        emrIds.push(_recordId);
+        
+        emit EMRCreated(owner(), _recordId);
+    }
+
+    function voidEMR(uint256 _emrID) public onlyOwner {
+        //Remove from _documents;
+    }
+
+    function getEMR(uint256 _emrID) public view onlyOwner returns(EMR memory) {
+        return emrs[_emrID];
+    }
+
+    function getEMRIDs() public view onlyOwner returns(uint256[] memory){
+        return emrIds;
+    }
+
+}
